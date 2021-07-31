3533ab244489c09b40767b317ac33a778b91019c
C107156116 <52107279+C107156116@users.noreply.github.com>
2021-07-31 20:12:21 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -10,14 +10,10 @@ from flask_cors import CORS
 from json import dumps
 from flask import Flask, make_response
 app = Flask(__name__)
-@app.route('/test')
 app.config['MYSQL_HOST']='remotemysql.com'
 app.config['MYSQL_USER']='GqD8cGeo5O'
 app.config['MYSQL_PASSWORD']='BKeOFOJ8xs'
 app.config['MYSQL_DB']='GqD8cGeo5O'
-
-
-
 mysql=MySQL(app)
 
 cols=['Age','Attrition','BusinessTravel','DailyRate','Department','DistanceFromHome','Education','EducationField','EmployeeCount','EmployeeNumber','EnvironmentSatisfaction','Gender','HourlyRate','JobInvolvement','JobLevel','JobRole','JobSatisfaction','MaritalStatus','MonthlyIncome','MonthlyRate','NumCompaniesWorked','Over18','OverTime','PercentSalaryHike','PerformanceRating','RelationshipSatisfaction','StandardHours','StockOptionLevel','TotalWorkingYears','TrainingTimesLastYear','WorkLifeBalance','YearsAtCompany','YearsInCurrentRole','YearsSinceLastPromotion','YearsWithCurrManager']
